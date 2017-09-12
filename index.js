38feb6fafe7258e7201509ec434e42d87c3559a0
Aly Pilkons <apilkons@brooksbell.com>
2017-09-12 13:07:00 -0400

    added gitignore and procfile files

--- index.js
+++ index.js
@@ -49,6 +49,6 @@ app.get('/:time', function (request, response) {
   response.json(jsonToReturn);
 });
 
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('listening...');
 });
